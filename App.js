40bfc05832fcf242f92250de01f35ae88a154bd4
yoidea <yoidea.cc@gmail.com>
2018-10-22 21:54:26 +0900

    add Navigation

--- App.js
+++ App.js
@@ -1,57 +1,49 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Button, Text } from 'native-base';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            console.log(this.props.navigation);
+            return this.props.navigation.navigate('Details');
+          }}
+          />
+        <Text>{this.props.navigation}</Text>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+        title='push Home'
+        onPress={() => this.props.navigation.push('Home')}
+        />
+      </View>
+      )
+  }
+}
+
+const RootStack = createStackNavigator(
+{
+  Home: HomeScreen,
+  Details: DetailsScreen,
+},
+{
+  initialRouteName: 'Home',
+}
+);
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isReady: false
-    };
-  }
-
-  componentWillMount() {
-    this.loadFonts();
-  }
-
-  async loadFonts() {
-    await Expo.Font.loadAsync({
-      Roboto: require("./node_modules/native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("./node_modules/native-base/Fonts/Roboto_medium.ttf"),
-    });
-    this.setState({ isReady: true });
-  }
-
   render() {
-    if (!this.state.isReady) {
-      return <Expo.AppLoading />;
-    }
-    return (
-        <Container>
-          <Header />
-          <Content>
-            <Button rounded light>
-              <Text>Light</Text>
-            </Button>
-            <Button rounded>
-              <Text>Primary</Text>
-            </Button>
-            <Button rounded success>
-              <Text>Success</Text>
-            </Button>
-            <Button rounded info>
-              <Text>Info</Text>
-            </Button>
-            <Button rounded warning>
-              <Text>Warning</Text>
-            </Button>
-            <Button rounded danger>
-              <Text>Danger</Text>
-            </Button>
-            <Button rounded dark>
-              <Text>Dark</Text>
-            </Button>
-          </Content>
-        </Container>
-    );
+    return <RootStack />;
   }
 }
