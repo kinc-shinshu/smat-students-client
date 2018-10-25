78c28f5dd6e2d91edcf5925c121f3b8839ae20e7
yoidea <yoidea.cc@gmail.com>
2018-10-25 18:29:13 +0900

    Add parser

--- screens/Apple.js
+++ screens/Apple.js
@@ -3,33 +3,55 @@ import { Container, Content, List, ListItem, Text, Left, Right, Icon} from 'nati
 import MathJax from 'react-native-mathjax';
 
 export class Apple extends Component {
+  parse = (text) => {
+    let result = text.replace(/\*/g, "\\times");
+    result = result.replace(/\//g, "\\div");
+    result = result.replace(/\+-/g, "\\pm");
+    result = result.replace(/-\+/g, "\\mp");
+    const ss = result.match(/#{.+?}/g);
+    if (ss != null){
+      for (let s of ss){
+        let k = s.match(/[^#]+/g);
+        result = result.replace(s, "\\sqrt{" + k[0].slice(1, -1) + "}");
+      }
+    }
+    const fs = result.match(/\[.+?]%\[.+?]/g);
+    if (fs != null){
+      for (let f of fs){
+        console.log(f);
+        let k = f.match(/[^%]+/g);
+        result = result.replace(f, "\\frac{" + k[0].slice(1, -1) + "}{" + k[1].slice(1, -1) + "}");
+      }
+    }
+    result = result.replace(/@|#|\$|%|&/, "");
+    console.log(result);
+    return result
+  }
+
   render() {
     const questions = this.props.navigation.state.params.questions;
-    const question = [questions[this.props.navigation.state.params.questionId]];
+    const question = questions[this.props.navigation.state.params.questionId];
     const nextQuestion = [questions[this.props.navigation.state.params.questionId + 1]];
-    const items = question.map((c, i) => {
-      return (
-        <MathJax
-          // HTML content with MathJax support
-          html={'$\\sqrt{2}$'}
-          // MathJax config option
-          mathJaxOptions={{
-            messageStyle: 'none',
-            extensions: [ 'tex2jax.js' ],
-            jax: [ 'input/TeX', 'output/HTML-CSS' ],
-            tex2jax: {
-              inlineMath: [ ['$','$'], ['\\(','\\)'] ],
-              displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
-              processEscapes: true,
-            },
-            TeX: {
-              extensions: ['AMSmath.js','AMSsymbols.js','noErrors.js','noUndefined.js']
-            }
-          }}
-        />
-
-      );
-    });
+    const items = (
+      <MathJax
+        // HTML content with MathJax support
+        html={'$' + this.parse(question.text) + '$'}
+        // MathJax config option
+        mathJaxOptions={{
+          messageStyle: 'none',
+          extensions: [ 'tex2jax.js' ],
+          jax: [ 'input/TeX', 'output/HTML-CSS' ],
+          tex2jax: {
+            inlineMath: [ ['$','$'], ['\\(','\\)'] ],
+            displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+            processEscapes: true,
+          },
+          TeX: {
+            extensions: ['AMSmath.js','AMSsymbols.js','noErrors.js','noUndefined.js']
+          }
+        }}
+      />
+    );
     return (
       <Container>
         <Content>
