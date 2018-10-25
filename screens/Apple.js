5a8ced3b46f3e663664d14b5bdd5ee25fc63be80
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-10-25 17:59:20 +0900

    addTexToApple

--- screens/Apple.js
+++ screens/Apple.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Content, List, ListItem, Text, Left, Right, Icon} from 'native-base';
+import MathJax from 'react-native-mathjax';
 
 export class Apple extends Component {
   render() {
@@ -8,35 +9,31 @@ export class Apple extends Component {
     const nextQuestion = [questions[this.props.navigation.state.params.questionId + 1]];
     const items = question.map((c, i) => {
       return (
-        <ListItem key={i} questionId={c.id} onPress={() => {this.props.navigation.navigate('BigList');}} >
-          <Left>
-            <Text>{c.id}</Text>
-          </Left>
-          <Right>
-            <Icon name="arrow-forward" />
-          </Right>
-        </ListItem>
-      );
-    });
-    const items1 = nextQuestion.map((c, i) => {
-      return (
-        <ListItem key={i} questionId={c.id} onPress={() => {this.props.navigation.navigate('BigList');}} >
-          <Left>
-            <Text>{c.id}</Text>
-          </Left>
-          <Right>
-            <Icon name="arrow-forward" />
-          </Right>
-        </ListItem>
+        <MathJax
+          // HTML content with MathJax support
+          html={'$\\sqrt{2}$'}
+          // MathJax config option
+          mathJaxOptions={{
+            messageStyle: 'none',
+            extensions: [ 'tex2jax.js' ],
+            jax: [ 'input/TeX', 'output/HTML-CSS' ],
+            tex2jax: {
+              inlineMath: [ ['$','$'], ['\\(','\\)'] ],
+              displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+              processEscapes: true,
+            },
+            TeX: {
+              extensions: ['AMSmath.js','AMSsymbols.js','noErrors.js','noUndefined.js']
+            }
+          }}
+        />
+
       );
     });
     return (
       <Container>
         <Content>
-          <List>
-            {items}
-            {items1}
-          </List>
+          {items}
         </Content>
       </Container>
     );
